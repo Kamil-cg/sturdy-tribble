80389ad3cdcda4946f753c78d267132907b089ac
Kamil-cg <kamil.kowalczyk@capgemini.com>
2026-02-19 13:05:01 +0000

    usuniecie starego build

---
+++ app/customer/dist/Component.js
@@ -0,0 +1,2 @@
+sap.ui.define(["sap/fe/core/AppComponent"],function(t){"use strict";return t.extend("btp.starterkit.customer.Component",{metadata:{manifest:"json"}})});
+//# sourceMappingURL=Component.js.map
